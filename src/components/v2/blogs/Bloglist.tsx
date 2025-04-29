4f08a71e201050231afd0272336ac0d667640aec
KunalNasa <kunalnasa.dev@gmail.com>
2025-04-30 00:25:43 +0530

    fix blog section

--- src/components/v2/blogs/Bloglist.tsx
+++ src/components/v2/blogs/Bloglist.tsx
@@ -19,9 +19,9 @@ export default function BlogList({ posts }: { posts: Post[] }) {
           href={`https://kunalnasa.hashnode.dev/${post.slug}`}
           target="_blank"
         >   
-          <div className="px-4 rounded-xl shadow-inner my-2 border-gray-300 dark:border-gray-700 hover:shadow-gray-500 transition duration-200 max-h-40 overflow-hidden border py-1">
+          <div className="p-3 rounded-xl shadow-inner my-2 border-gray-300 dark:border-gray-700 hover:shadow-gray-500 transition duration-200 max-h-40 overflow-auto border  min-h-40">
             <h2 className="text-lg font-semibold">{post.title}</h2>
-            <p className="text-sm text-gray-500 mt-2">{post.brief}</p>
+            <p className="text-sm text-gray-500 mt-2">{post.brief.slice(0, 70)}...</p>
           </div>
         </Link>
       ))}
