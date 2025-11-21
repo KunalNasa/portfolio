e8381ed12e8f291d2e5f86f70bff709aeae18ce9
kunal <kunalnasa.dev@gmail.com>
2025-11-22 01:34:48 +0530

    update site domain names

--- src/helpers/Project.ts
+++ src/helpers/Project.ts
@@ -1,63 +1,63 @@
 export const projects = [
-    {
-        imageUrl: '/Images/SkillForgeLogo.png',
-        title: 'SkillForge',
-        liveUrl: "https://skillforge.kunalnasa.xyz/",
-        description: 'AI-powered tool for generating personalized learning roadmaps with progress tracking.',
-        githubUrl: 'https://github.com/KunalNasa/skillforge',
-        techstack: ['MongoDB', 'Next.js', 'Zod', 'Gemini API', 'Shadcn UI']
-    },
-    {
-        imageUrl: '/Images/slates.jpeg',
-        title: 'Slates',
-        description: 'Real-time collaborative canvas board with feature of zooming, panning and more.',
-        githubUrl: 'https://github.com/KunalNasa/slates',
-        techstack: ['Postgres', 'Next.js', 'Express.js',  'Zod', 'Web sockets', "BullMQ", "Redis", "Turborepo"]
-    },
-    {
-      imageUrl: '/Images/wiseupNew.jpeg',
-      title: 'WiseUp',
-      liveUrl: "https://wiseup.kunalnasa.xyz",
-      description: 'SaaS app for managing monthly transactions and visualizing spending through charts.',
-      githubUrl: 'https://github.com/KunalNasa/WiseUp',
-      techstack: ['PostgreSQL', 'Next.js', 'Zod', 'Clerk', 'Shadcn UI', 'Stripe', 'Web-hooks', 'NeonDB']
-    },
-    {
-        imageUrl: '/Images/go0-nNew.png',
-        title: 'Go Zero to N',
-        liveUrl: "https://go0-n.com",
-        description: 'A platform connecting companies with recruiters to simplify hiring and talent sourcing.',
-        techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI', 'Aceternity UI', 'Axios']
-    },
-    {
-      imageUrl: '/Images/wetalk.png',
-      title: 'WeTalk',
-      liveUrl: "https://wetalk-2vvh.onrender.com/",
-      description: 'Real-time chat app using Socket.IO and MERN stack with instant messaging features.',
-      githubUrl: 'https://github.com/KunalNasa/ChatApp',
-      techstack: ['MongoDB', 'React.js', 'Express.js', 'Zod', 'Socket.io', 'Daisy UI', 'Zustand']
-    },
-    {
-      imageUrl: '/Images/FeedBackApp.png',
-      title: 'SendMeAFeedback',
-      liveUrl: "https://send-me-a-feedback.vercel.app/",
-      description: 'Anonymous feedback tool with intelligent Gemini API responses and privacy focus.',
-      githubUrl: 'https://github.com/KunalNasa/send-me-a-feedback',
-      techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
-    },
-    {
-      imageUrl: '/Images/GDlogo.png',
-      title: 'Green Dash',
-      description: 'Sustainability dashboard with a carbon footprint calculator to promote eco habits.',
-      githubUrl: 'https://github.com/KunalNasa/SustainableLivingDash',
-      techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
-    },
-    {
-      imageUrl: '/Images/spotifyLogo.svg',
-      title: 'Spotify Clone',
-      description: 'Responsive music app built with Vanilla JS featuring audio playback and playlists.',
-      githubUrl: 'https://github.com/KunalNasa/Web-Dev/tree/main/Spotify',
-      techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
-    },
-  ];
-  
+  {
+    imageUrl: '/Images/SkillForgeLogo.png',
+    title: 'SkillForge',
+    liveUrl: "https://skillforge.kunalnasa.work/",
+    description: 'AI-powered tool for generating personalized learning roadmaps with progress tracking.',
+    githubUrl: 'https://github.com/KunalNasa/skillforge',
+    techstack: ['MongoDB', 'Next.js', 'Zod', 'Gemini API', 'Shadcn UI']
+  },
+  {
+    imageUrl: '/Images/slates.jpeg',
+    title: 'Slates',
+    description: 'Real-time collaborative canvas board with feature of zooming, panning and more.',
+    githubUrl: 'https://github.com/KunalNasa/slates',
+    techstack: ['Postgres', 'Next.js', 'Express.js', 'Zod', 'Web sockets', "BullMQ", "Redis", "Turborepo"]
+  },
+  {
+    imageUrl: '/Images/wiseupNew.jpeg',
+    title: 'WiseUp',
+    liveUrl: "https://wiseup.kunalnasa.work",
+    description: 'SaaS app for managing monthly transactions and visualizing spending through charts.',
+    githubUrl: 'https://github.com/KunalNasa/WiseUp',
+    techstack: ['PostgreSQL', 'Next.js', 'Zod', 'Clerk', 'Shadcn UI', 'Stripe', 'Web-hooks', 'NeonDB']
+  },
+  {
+    imageUrl: '/Images/go0-nNew.png',
+    title: 'Go Zero to N',
+    liveUrl: "https://go0-n.com",
+    description: 'A platform connecting companies with recruiters to simplify hiring and talent sourcing.',
+    techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI', 'Aceternity UI', 'Axios']
+  },
+  {
+    imageUrl: '/Images/wetalk.png',
+    title: 'WeTalk',
+    liveUrl: "https://wetalk-2vvh.onrender.com/",
+    description: 'Real-time chat app using Socket.IO and MERN stack with instant messaging features.',
+    githubUrl: 'https://github.com/KunalNasa/ChatApp',
+    techstack: ['MongoDB', 'React.js', 'Express.js', 'Zod', 'Socket.io', 'Daisy UI', 'Zustand']
+  },
+  {
+    imageUrl: '/Images/FeedBackApp.png',
+    title: 'SendMeAFeedback',
+    liveUrl: "https://send-me-a-feedback.vercel.app/",
+    description: 'Anonymous feedback tool with intelligent Gemini API responses and privacy focus.',
+    githubUrl: 'https://github.com/KunalNasa/send-me-a-feedback',
+    techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
+  },
+  {
+    imageUrl: '/Images/GDlogo.png',
+    title: 'Green Dash',
+    description: 'Sustainability dashboard with a carbon footprint calculator to promote eco habits.',
+    githubUrl: 'https://github.com/KunalNasa/SustainableLivingDash',
+    techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
+  },
+  {
+    imageUrl: '/Images/spotifyLogo.svg',
+    title: 'Spotify Clone',
+    description: 'Responsive music app built with Vanilla JS featuring audio playback and playlists.',
+    githubUrl: 'https://github.com/KunalNasa/Web-Dev/tree/main/Spotify',
+    techstack: ['MongoDB', 'Next.js', 'Zod', 'Nodemailer', 'Shadcn UI']
+  },
+];
+
